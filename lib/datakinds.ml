480888141c94fe7be2c66ad819db4d78fb3d1878
johana <johan.alm@who-umc.org>
2026-02-19 16:13:03 +0100

    Liking the looks of this datakinds system.

--- lib/datakinds.ml
+++ lib/datakinds.ml
@@ -7,44 +7,30 @@ open Common.Identifiers
 
 let ( let* ) = Result.bind
 
-type polarity = Positive | Negative
-
 (* Types *)
 type typ =
-    Pos (* Kind of positive types *)
-  | Neg (* Kind of negative types *)
-  | Arrow of typ * typ (* Only for kinds *)
-  | Int (* Primitive type of integers; is positive *)
+  | Typ
+  | Int
   | TVar of Ident.t
   | TMeta of Ident.t
-  | Sgn of Path.t * typ list (* Applied data or codata type *)
+  | Arrow of typ * typ
+  | Data of Path.t * typ list
   | PromotedCtor of Path.t * Path.t * typ list
 
-let as_typ = function Positive -> Pos | Negative -> Neg
-
-type chiral_typ = Prd of typ | Cns of typ
-
-let strip_chirality = function Prd t | Cns t -> t
-
-let chiral_map f = function Prd t -> Prd (f t) | Cns t -> Cns (f t)
-
-(* Constructor or destructor *)
-type xtor =
-  { name: Path.t
-  ; quantified: (Ident.t * typ) list
-  ; existential: (Ident.t * typ) list
-  ; argument_types: chiral_typ list
-  (* `main` is the result type of data types and the "this"
-    type of codata types *)
-  ; main: typ
+(* Constructor *)
+type ctor =
+  { name : Path.t
+  ; quantified : (Ident.t * typ) list
+  ; existential : (Ident.t * typ) list
+  ; argument_types : typ list
+  ; result : typ
   }
 
 (* Declaration *)
 type dec =
-  { name: Path.t
-  ; polarity: polarity
-  ; param_kinds: typ list
-  ; xtors: xtor list
+  { name : Path.t
+  ; param_kinds : typ list
+  ; ctors : ctor list
   }
 
 (* Promoted constructor info *)
@@ -56,34 +42,40 @@ type promoted_ctor_info =
 
 (* Kind errors *)
 type kind_error =
-    Unbound_type_variable of Ident.t
+  | Unbound_type_variable of Ident.t
   | Unbound_meta_variable of Ident.t
   | Unknown_data_type of Path.t
   | Unknown_promoted_ctor of Path.t * Path.t
   | Not_a_promoted_type of Path.t
   | Invalid_kind of typ
   | Kind_mismatch of { expected: typ; actual: typ; in_type: typ }
-  | Arity_mismatch of { kind: typ option; num_args: int }
+  | Arity_mismatch of { kind: typ; num_args: int }
   | Arrow_domain_not_typ of typ
   | Arrow_codomain_not_typ of typ
   | Too_many_arguments of { kind: typ; extra_args: typ list }
 
+(* Wrapper for Ident.find_opt returning result *)
 let ident_find (v: Ident.t) (tbl: 'a Ident.tbl) (err: kind_error) : ('a, kind_error) result =
-  match Ident.find_opt v tbl with Some x -> Ok x | None -> Error err
+  match Ident.find_opt v tbl with
+  | Some x -> Ok x
+  | None -> Error err
 
+(* Wrapper for Path.find_opt returning result *)
 let path_find (p: Path.t) (tbl: 'a Path.tbl) (err: kind_error) : ('a, kind_error) result =
-  match Path.find_opt p tbl with Some x -> Ok x | None -> Error err
+  match Path.find_opt p tbl with
+  | Some x -> Ok x
+  | None -> Error err
 
 (* Substitution *)
 type subst = typ Ident.tbl
 
 (* Context *)
 type context =
-  { decs: dec Path.tbl
-  ; data_kinds: typ Path.tbl
-  ; promoted_ctors: promoted_ctor_info Path.tbl
-  ; subst: subst
-  ; typ_vars: typ Ident.tbl
+  { decs : dec Path.tbl
+  ; data_kinds : typ Path.tbl
+  ; promoted_ctors : promoted_ctor_info Path.tbl
+  ; subst : subst
+  ; typ_vars : typ Ident.tbl
   }
 
 (* Create an empty context *)
@@ -103,7 +95,7 @@ let rec apply_subst sbs t =
       | None -> t)
   | TVar _ -> t
   | Arrow (t1, t2) -> Arrow (apply_subst sbs t1, apply_subst sbs t2)
-  | Sgn (name, args) -> Sgn (name, List.map (apply_subst sbs) args)
+  | Data (name, args) -> Data (name, List.map (apply_subst sbs) args)
   | PromotedCtor (data_name, ctor_name, args) ->
       PromotedCtor (data_name, ctor_name, List.map (apply_subst sbs) args)
   | _ -> t
@@ -113,7 +105,7 @@ let rec occurs v t =
     TVar v' -> Ident.equal v v'
   | TMeta v' -> Ident.equal v v'
   | Arrow (t1, t2) -> occurs v t1 || occurs v t2
-  | Sgn (_, args) -> List.exists (occurs v) args
+  | Data (_, args) -> List.exists (occurs v) args
   | PromotedCtor (_, _, args) -> List.exists (occurs v) args
   | _ -> false
 
@@ -136,9 +128,8 @@ let rec unify t1 t2 sbs =
   | Arrow (a1, b1), Arrow (a2, b2) ->
       Option.bind (unify a1 a2 sbs) (unify b1 b2)
   | Int, Int -> Some sbs
-  | Pos, Pos -> Some sbs
-  | Neg, Neg -> Some sbs
-  | Sgn (name1, args1), Sgn (name2, args2) when
+  | Typ, Typ -> Some sbs
+  | Data (name1, args1), Data (name2, args2) when
         Path.equal name1 name2 &&
         List.length args1 = List.length args2 ->
       fold_zip (Some sbs) (args1, args2)
@@ -175,25 +166,21 @@ let rec apply_fresh_subst sbs t =
       (match Ident.find_opt v sbs with Some t' -> t' | None -> t)
   | Arrow (t1, t2) ->
       Arrow (apply_fresh_subst sbs t1, apply_fresh_subst sbs t2)
-  | Sgn (name, args) ->
-      Sgn (name, List.map (apply_fresh_subst sbs) args)
+  | Data (name, args) ->
+      Data (name, List.map (apply_fresh_subst sbs) args)
   | PromotedCtor (d, c, args) ->
       PromotedCtor (d, c, List.map (apply_fresh_subst sbs) args)
   | _ -> t
 
 (* A constructor is promotable (can be lifted to the type/kind level) if:
   1. It has no existential variables
-  2. All its argument types are producer types (Prd)
-  3. Its result type is the canonical form: Sgn(typeName, [TVar q1, TVar q2, ...])
+  2. Its result type is the canonical form: Data(typeName, [TVar q1, TVar q2, ...])
     where the variables are exactly the quantified variables, in order. *)
-let is_promotable type_name (ctor: xtor) : bool =
+let is_promotable type_name (ctor: ctor) : bool =
   let no_existentials = (ctor.existential = []) in
-  let only_producer_args = List.for_all (
-    function Prd _ -> true | Cns _ -> false
-  ) ctor.argument_types in
   let canonical_result =
-    match ctor.main with
-    | Sgn (name, args) when Path.equal name type_name ->
+    match ctor.result with
+    | Data (name, args) when Path.equal name type_name ->
         List.length args = List.length ctor.quantified &&
         List.for_all2 (fun arg (qvar, _) ->
           match arg with
@@ -201,13 +188,13 @@ let is_promotable type_name (ctor: xtor) : bool =
           | _ -> false
         ) args ctor.quantified
     | _ -> false
-  in no_existentials && only_producer_args && canonical_result
+  in
+  no_existentials && canonical_result
 
 (* Check if a declaration can serve as a DataKind (if all constructors
   are promotable) *)
 let is_dec_promotable (dec: dec) : bool =
-  dec.polarity = Positive && (* Only data types are promotable *)
-  List.for_all (is_promotable dec.name) dec.xtors
+  List.for_all (is_promotable dec.name) dec.ctors
 
 (* Helper to sequence result checks over a list *)
 let rec check_all f = function
@@ -219,25 +206,24 @@ let rec check_all2 f xs ys =
   match xs, ys with
   | [], [] -> Ok ()
   | x :: xs', y :: ys' -> let* _ = f x y in check_all2 f xs' ys'
-  | _ -> Error (Arity_mismatch { kind = None; num_args = 0 })
+  | _ -> Error (Arity_mismatch { kind = Typ; num_args = 0 })
 
 (* Check if a type is a valid kind (can classify types) *)
 let rec valid_kind (ctx: context) (t: typ) : (unit, kind_error) result =
   match t with
-  | Pos | Neg -> Ok ()
+  | Typ -> Ok ()
   | Arrow (k1, k2) -> let* _ = valid_kind ctx k1 in valid_kind ctx k2
-  | Sgn (name, args) ->
+  | Data (name, args) ->
       let* kind =
         path_find name ctx.data_kinds (Not_a_promoted_type name) in
       let* _ = check_all (valid_kind ctx) args in
       let rec arity k =
         match k with Arrow (_, r) -> 1 + arity r | _ -> 0 in
       (match kind, args with
-      | Pos, [] | Neg, [] -> Ok ()
+      | Typ, [] -> Ok ()
       | Arrow _, _ when List.length args <= arity kind -> Ok ()
       | _, [] -> Ok ()
-      | _, _ -> Error (Arity_mismatch {
-        kind = Some kind; num_args = List.length args }))
+      | _, _ -> Error (Arity_mismatch { kind; num_args = List.length args }))
   | TVar v ->
       let* k = ident_find v ctx.typ_vars (Unbound_type_variable v) in
       valid_kind ctx k
@@ -249,9 +235,7 @@ let rec valid_kind (ctx: context) (t: typ) : (unit, kind_error) result =
 (* Infer the kind of a type *)
 let rec infer_kind (ctx: context) (t: typ) : (typ, kind_error) result =
   match t with
-  | Pos -> Ok Pos
-  | Neg -> Ok Neg
-  | Int -> Ok Pos
+  | Int -> Ok Typ
   | TVar v -> ident_find v ctx.typ_vars (Unbound_type_variable v)
   | TMeta v ->
       (match Ident.find_opt v ctx.subst with
@@ -260,16 +244,15 @@ let rec infer_kind (ctx: context) (t: typ) : (typ, kind_error) result =
   | Arrow (t1, t2) ->
       let* k1 = infer_kind ctx t1 in
       let* k2 = infer_kind ctx t2 in
-      (* Arrow types at the kind level: both must be valid kinds *)
-      let* _ = valid_kind ctx k1 in
-      let* _ = valid_kind ctx k2 in
-      Ok Neg (* Arrow types are negative *)
-  | Sgn (name, args) ->
+      if equiv ctx.subst k1 Typ then
+        if equiv ctx.subst k2 Typ then Ok Typ
+        else Error (Arrow_codomain_not_typ t2)
+      else Error (Arrow_domain_not_typ t1)
+  | Data (name, args) ->
       let* dec = path_find name ctx.decs (Unknown_data_type name) in
       let full_kind = List.fold_right (fun k acc ->
         Arrow (k, acc)
-      ) dec.param_kinds (as_typ dec.polarity)
-      in
+      ) dec.param_kinds Typ in
       apply_args ctx full_kind args
   | PromotedCtor (data_name, ctor_name, args) ->
       let key = Path.access data_name (Path.name ctor_name) in
@@ -281,7 +264,7 @@ let rec infer_kind (ctx: context) (t: typ) : (typ, kind_error) result =
       let expected_args = num_quantified + num_kind_args in
       if List.length args <> expected_args then
         Error (Arity_mismatch {
-          kind = Some info.result_kind; num_args = List.length args })
+          kind = info.result_kind; num_args = List.length args })
       else
         let type_args = List.filteri (fun i _ -> i < num_quantified) args in
         let kind_args = List.filteri (fun i _ -> i >= num_quantified) args in
@@ -300,6 +283,7 @@ let rec infer_kind (ctx: context) (t: typ) : (typ, kind_error) result =
         ) subst_arg_kinds kind_args in
         let subst_result = apply_fresh_subst ty_subst info.result_kind in
         Ok subst_result
+  | Typ -> Ok Typ
 
 (* Apply type arguments to a kind, returning the resulting kind *)
 and apply_args (ctx: context) (kind: typ) (args: typ list)
@@ -319,27 +303,21 @@ and check_kind (ctx: context) (t: typ) (expected_kind: typ)
   else Error (Kind_mismatch {
     expected = expected_kind; actual = inferred_kind; in_type = t })
 
-and is_inhabitable (ctx: context) (t: typ) : bool =
-  match check_kind ctx t Pos with
-  | Ok () -> true
-  | Error _ -> Result.is_ok (check_kind ctx t Neg)
-
-(* Check that a constructor or destructor is well-kinded *)
-let check_xtor_well_kinded (ctx: context) (pol: polarity) (xtor: xtor) : bool =
+(* Check that a constructor is well-kinded *)
+let check_ctor_well_kinded (ctx: context) (ctor: ctor) : bool =
   let ty_ctx =
     List.fold_left (fun acc (v, k) ->
         { acc with typ_vars = Ident.add v k acc.typ_vars }
-      ) ctx (xtor.quantified @ xtor.existential)
+      ) ctx (ctor.quantified @ ctor.existential)
   in
   (* kinds_valid && args_valid && result_valid *)
   List.for_all (fun (_, k) ->
     Result.is_ok (valid_kind ty_ctx k)
-  ) (xtor.quantified @ xtor.existential) &&
-  List.for_all (fun ct ->
-    (* Argument types must be inhabitable (kind + or -) *)
-    is_inhabitable ty_ctx (strip_chirality ct)
-  ) xtor.argument_types &&
-  Result.is_ok (check_kind ty_ctx xtor.main (as_typ pol))
+  ) (ctor.quantified @ ctor.existential) &&
+  List.for_all (fun t ->
+    Result.is_ok (check_kind ty_ctx t Typ)
+  ) ctor.argument_types &&
+  Result.is_ok (check_kind ty_ctx ctor.result Typ)
 
 (* Check that a declaration is well-kinded *)
 let check_dec_well_kinded (ctx: context) (dec: dec) : bool =
@@ -348,28 +326,28 @@ let check_dec_well_kinded (ctx: context) (dec: dec) : bool =
       Result.is_ok (valid_kind ctx k)
     ) dec.param_kinds in
   let ctors_valid =
-    List.for_all (check_xtor_well_kinded ctx dec.polarity) dec.xtors in
+    List.for_all (check_ctor_well_kinded ctx) dec.ctors in
   param_kinds_valid && ctors_valid
 
 (* Build PromotedCtorInfo for a promotable constructor *)
-let build_promoted_ctor_info (ctor: xtor) : promoted_ctor_info =
+let build_promoted_ctor_info (ctor: ctor) : promoted_ctor_info =
   { quantified = ctor.quantified
-  ; arg_kinds = List.map strip_chirality ctor.argument_types
-  ; result_kind = ctor.main
+  ; arg_kinds = ctor.argument_types
+  ; result_kind = ctor.result
   }
 
 (* Build promoted constructor entries for a declaration (only if all
   ctors are promotable) *)
 let build_promoted_ctors (d: dec) : (Path.t * promoted_ctor_info) list =
   if is_dec_promotable d then
-    List.map (fun (c: xtor) ->
+    List.map (fun (c: ctor) ->
       (Path.access d.name (Path.name c.name), build_promoted_ctor_info c)
-    ) d.xtors
+    ) d.ctors
   else []
 
 (* Compute the kind of a data type from its param_kinds *)
 let data_kind_from_param_kinds (param_kinds: typ list) : typ =
-  List.fold_right (fun k acc -> Arrow (k, acc)) param_kinds Pos
+  List.fold_right (fun k acc -> Arrow (k, acc)) param_kinds Typ
 
 (* Add a declaration to the context *)
 let add_dec (ctx: context) (dec: dec) : context =
@@ -415,20 +393,20 @@ let add_declarations_recursive (ctx: context) (decs: dec list) : context option 
   else Some temp_ctx
 
 (* Check if a constructor is reachable given scrutinee type arguments *)
-let is_xtor_reachable (ctx: context) (dec: dec) (xtor: xtor) (scrutinee_args: typ list)
+let is_ctor_reachable (ctx: context) (dec: dec) (ctor: ctor) (scrutinee_args: typ list)
     : subst option =
-  let _, fresh_subst = freshen_meta xtor.quantified in
-  let fresh_result = apply_fresh_subst fresh_subst xtor.main in
-  let scrutinee_type = Sgn (dec.name, scrutinee_args) in
+  let _, fresh_subst = freshen_meta ctor.quantified in
+  let fresh_result = apply_fresh_subst fresh_subst ctor.result in
+  let scrutinee_type = Data (dec.name, scrutinee_args) in
   unify fresh_result scrutinee_type ctx.subst
 
 (* Check exhaustivity: all reachable constructors must be covered *)
 let check_exhaustive
     (ctx: context) (d: dec) (scrutinee_args: typ list) (covered_ctors: Path.t list)
     : Path.t list =
-  List.filter_map (fun (x: xtor) ->
-    if not (List.exists (Path.equal x.name) covered_ctors) &&
-       Option.is_some (is_xtor_reachable ctx d x scrutinee_args)
-    then Some x.name
+  List.filter_map (fun (c: ctor) ->
+    if not (List.exists (Path.equal c.name) covered_ctors) &&
+       Option.is_some (is_ctor_reachable ctx d c scrutinee_args)
+    then Some c.name
     else None
-  ) d.xtors
+  ) d.ctors
